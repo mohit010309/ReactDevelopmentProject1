3ba53f7a446791211615a407a73d3a838e0dce22
mohit010309 <mohitakhouri83@gmail.com>
2025-08-13 02:40:54 +0530

    import statement used for image for better optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import coreConceptsPng from './assets/react-core-concepts.png';
 const ContentList =['Fundamentals','core','crucial'];
 
 function GetRandomIndex()
@@ -10,7 +11,7 @@ function Header() {
   const paraHeading = ContentList[GetRandomIndex()];
   return (
     <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={coreConceptsPng} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
           {paraHeading} React concepts you will need for almost any app you are
