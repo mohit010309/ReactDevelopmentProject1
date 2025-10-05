e75b3993d0f2bb177b2b2ad169f227d702408deb
mohit010309 <mohitakhouri83@gmail.com>
2025-10-06 01:53:57 +0530

    usestate implemented, key added in coreconcept for runtime error

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,24 @@ import Header from './components/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import { CORE_CONCEPTS } from './data.js'; 
 import { EXAMPLES } from './data.js';
+import {useState} from 'react';
 
 function App() {
+  const [selectedTopic,setSelectedTopic] = useState();
+
+  var tabContent = 'Please select a topic!';
+  if(selectedTopic)
+  {
+    tabContent = (
+      <div id='tab-content'>
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    )
+  }
   return (
     <div>
       <Header/>
@@ -12,7 +28,7 @@ function App() {
           <h2>CoreConcepts</h2>
           <ul>
             {CORE_CONCEPTS.map((concept) => (
-              <CoreConcept id={concept.title} {...concept}/>
+              <CoreConcept key = {concept.title} id={concept.title} {...concept}/>
             ))}
           </ul>
         </section>
@@ -20,19 +36,13 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <li><button>Components</button></li>
-            <li><button>JSX</button></li>
-            <li><button>Props</button></li>
-            <li><button>State</button></li>
+            <li><button className={selectedTopic=="components" ? 'active':undefined} onClick={()=>(setSelectedTopic('components'))}>Components</button></li>
+            <li><button className={selectedTopic=="jsx" ? 'active':undefined} onClick={()=>(setSelectedTopic('jsx'))}>JSX</button></li>
+            <li><button className={selectedTopic=="props" ? 'active':undefined} onClick={()=>(setSelectedTopic('props'))}>Props</button></li>
+            <li><button className={selectedTopic=="state" ? 'active':undefined} onClick={()=>(setSelectedTopic('state'))}>State</button></li>
           </menu>
 
-          <div id='tab-content'>
-            <h3>{EXAMPLES.components.title}</h3>
-            <p>{EXAMPLES.components.description}</p>
-            <pre>
-              <code>{EXAMPLES.components.code}</code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
